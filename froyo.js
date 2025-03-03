c3e23c288239ec933ef458f8908bd592769ed05f
Evelyn Demera <eveinahi@icloud.com>
2025-03-03 15:57:00 -0600

    yes

---
+++ froyo.js
@@ -0,0 +1,4 @@
+// A visitor receives a prompt upon opening the website to enter a list of comma-separated froyo flavors.
+
+const flavors =  ["vanilla", "vanilla", "vanilla", "strawberry", "coffee", "coffee"]
+console.log("Flavors Array:", flavors)
